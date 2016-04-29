24ce10f4af6ff2ee131eab92e748906ca41d202c
sikB <sikhovski@icloud.com>
2016-04-29 11:18:49 -0400

    update

--- routesGuide/controller2.js
+++ routesGuide/controller2.js
@@ -1,4 +1,4 @@
-var viewsApp=angular.module('viewsApp', ['ngRoute', 'ngAnimate']);
+var viewsApp=angular.module('viewsApp', ['ngRoute', 'ngAnimate', 'LocalStorageModule']);
 
 viewsApp.config(function($routeProvider){
 	$routeProvider.when('/0',{
@@ -18,8 +18,11 @@ viewsApp.config(function($routeProvider){
 // {templateFileName="template" + $routeParams.template + '.html'});});
 // this makes it do all the work for you. instead of doing it like the above way..i would have to copy paste for every single
 // page that i want to make. this way it makes it with the function
-viewsApp.controller('viewsController', function($scope, $location){
+viewsApp.controller("viewsController", 
+ function ($scope, $location, localStorageService) {
+  	localStorageService.set('myGolfer','Zach');
 	$scope.nextId = function(id){
-		$location.path("/" +id);
+		console.log(id);
+		$location.path("/"+id);
 	}
-});
+} );
